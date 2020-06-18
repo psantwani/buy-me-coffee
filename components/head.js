46b7087757d8e75d961b9dc3caa2ecef33267787
Piyush Santwani <psantwani@gmail.com>
2020-06-18 13:36:06 +0530

    typo fix

--- components/head.js
+++ components/head.js
@@ -8,7 +8,7 @@ import 'bootstrap/scss/bootstrap.scss';
 export default function HeadMeta(){
     return (
         <Head>
-                <title>Buy Coffee to {profileData.name}</title>
+                <title>Buy Coffee for {profileData.name}</title>
                 <link rel="icon" href="/favicon.png" />
                 <script type="text/javascript" src="https://gumroad.com/js/gumroad.js" />
                 <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700&display=swap" rel="stylesheet" />
